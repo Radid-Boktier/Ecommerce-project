3c3a40eed8544c524fa7ac5b37b96dafa44bf668
Radid Ahammed <radidboktier823@gmail.com>
2025-11-15 00:22:02 +0600

    Add global middleware and integrate it into the project routing

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -3,6 +3,7 @@ package cmd
 import (
 	"ecommerce-server/global_router"
 	"ecommerce-server/handlers"
+	"ecommerce-server/middleware"
 	"fmt"
 	"net/http"
 )
@@ -10,11 +11,13 @@ import (
 func Serve() {
 	mux := http.NewServeMux() // router
 
-	mux.Handle("GET /products",http.HandlerFunc(handlers.GetProducts))
+	mux.Handle("GET /route", middleware.Logger(http.HandlerFunc(handlers.Test)))
 
-	mux.Handle("POST /products",http.HandlerFunc(handlers.CreateProduct))
+	mux.Handle("GET /products",middleware.Logger(http.HandlerFunc(handlers.GetProducts))) // route
 
-	mux.Handle("GET /products/{productID}",http.HandlerFunc(handlers.GetProductByID))
+	mux.Handle("POST /products",middleware.Logger(http.HandlerFunc(handlers.CreateProduct))) // route
+
+	mux.Handle("GET /products/{productID}",middleware.Logger(http.HandlerFunc(handlers.GetProductByID))) // route
 
 	fmt.Println("Server running on : 8080")
 
