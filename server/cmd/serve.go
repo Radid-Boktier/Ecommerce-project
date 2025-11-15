fe8a332a35cf130413feae344a62b5dd93791d78
Radid Ahammed <radidboktier823@gmail.com>
2025-11-15 20:17:45 +0600

    refactor middlewares

--- server/cmd/serve.go
+++ server/cmd/serve.go
@@ -9,15 +9,34 @@ import (
 )
 
 func Serve() {
+	manager := middleware.NewManager()
+
 	mux := http.NewServeMux() // router
 
-	mux.Handle("GET /route", middleware.Logger(http.HandlerFunc(handlers.Test)))
+	mux.Handle("GET /test", manager.With(
+		http.HandlerFunc(handlers.Test),
+		middleware.Logger,
+	))
 
-	mux.Handle("GET /products",middleware.Logger(http.HandlerFunc(handlers.GetProducts))) // route
+	mux.Handle("GET /route", manager.With(
+		http.HandlerFunc(handlers.Test),
+		middleware.Logger,
+	))
 
-	mux.Handle("POST /products",middleware.Logger(http.HandlerFunc(handlers.CreateProduct))) // route
+	mux.Handle("GET /products",manager.With(
+		http.HandlerFunc(handlers.GetProducts),
+		middleware.Logger,
+	)) // route
 
-	mux.Handle("GET /products/{productID}",middleware.Logger(http.HandlerFunc(handlers.GetProductByID))) // route
+	mux.Handle("POST /products",manager.With(
+		http.HandlerFunc(handlers.CreateProduct),
+		middleware.Logger,
+	)) // route
+
+	mux.Handle("GET /products/{productID}",manager.With(
+		http.HandlerFunc(handlers.GetProductByID),
+		middleware.Logger,
+	)) // route
 
 	fmt.Println("Server running on : 8080")
 
--- server/middleware/logger.go
+++ server/middleware/logger.go
@@ -10,9 +10,8 @@ func Logger(next http.Handler) http.Handler {
 	return  http.HandlerFunc(func (w http.ResponseWriter, r *http.Request){
 		start := time.Now()
 
-		log.Println("This is middleware: first")
 		next.ServeHTTP(w, r)
-		log.Println("This is middleware: last")
+		
 		log.Println(r.Method, r.URL.Path, time.Since(start))
 	})
 }
